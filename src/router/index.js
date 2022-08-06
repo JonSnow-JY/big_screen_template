8a28700a36a8e75db110fcaf14e0c28525642ba5
JonSnow <1571298579@qq.com>
2022-08-06 10:47:20 +0800

    no message

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,24 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
+const _import = require("@/libs/util.import." + process.env.NODE_ENV);
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "HomeView",
+    component: () => _import("HomeView"),
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/",
+    name: "AboutView",
+    component: () => _import("AboutView"),
   },
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  // mode: "history",
   base: process.env.BASE_URL,
   routes,
 });
